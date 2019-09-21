aebbfb00f3de571d9fb397328b47d850837383ba
L30N87 <54728140+L30N87@users.noreply.github.com>
2019-09-20 19:12:48 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,12 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/flask')
-def hello_flask():
-   return 'Hello Flask'
-
-@app.route('/python/')
-def hello_python():
-   return 'Hello Python'
-
-
+@app.route("/")
+def hello():
+    return "TELECOMUNICACIONES UTN"
