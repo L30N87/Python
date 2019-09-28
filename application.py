e641bf0433e5a4d1cb9678b865ddb05693720ba9
L30N87 <54728140+L30N87@users.noreply.github.com>
2019-09-28 09:16:37 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,210 +1,89 @@
-#Snake Tutorial Python
-
-import math
-import random
-import pygame
-import tkinter as tk
-from tkinter import messagebox
-
-class cube(object):
-    rows = 20
-    w = 500
-    def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
-        self.pos = start
-        self.dirnx = 1
-        self.dirny = 0
-        self.color = color
-
-        
-    def move(self, dirnx, dirny):
-        self.dirnx = dirnx
-        self.dirny = dirny
-        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
-
-    def draw(self, surface, eyes=False):
-        dis = self.w // self.rows
-        i = self.pos[0]
-        j = self.pos[1]
-
-        pygame.draw.rect(surface, self.color, (i*dis+1,j*dis+1, dis-2, dis-2))
-        if eyes:
-            centre = dis//2
-            radius = 3
-            circleMiddle = (i*dis+centre-radius,j*dis+8)
-            circleMiddle2 = (i*dis + dis -radius*2, j*dis+8)
-            pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
-            pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
-        
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+import os
+import uuid
+from azure.cosmos import CosmosClient
+from azure.cosmos.partition_key import PartitionKey
 
 
+class CosmosDB:
+    def __init__(self):
+        URL = os.environ["COSMOS_ENDPOINT"]
+        KEY = os.environ["COSMOS_KEY"]
+        self.client = CosmosClient(URL, {"masterKey": KEY})
+        self.dbName = "pySolarSystem-" + uuid.uuid1().hex
 
-class snake(object):
-    body = []
-    turns = {}
-    def __init__(self, color, pos):
-        self.color = color
-        self.head = cube(pos)
-        self.body.append(self.head)
-        self.dirnx = 0
-        self.dirny = 1
+    def create_database(self):
+        print("Creating '{0}' database...".format(self.dbName))
+        return self.client.create_database(self.dbName)
 
-    def move(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
+    def create_container(self, db):
+        collectionName = "Planets"
+        print("Creating '{0}' collection...".format(collectionName))
+        partition_key = PartitionKey(path="/id", kind="Hash")
+        return db.create_container(id="Planets", partition_key=partition_key)
 
-            keys = pygame.key.get_pressed()
+    def create_documents(self, container):
+        # Cosmos will look for an 'id' field in the items, if the 'id' is not specified, Cosmos is going to assign a random key.
+        planets = [
+            {
+                "id": "Earth",
+                "HasRings": False,
+                "Radius": 3959,
+                "Moons": [{"Name": "Moon"}],
+            },
+            {
+                "id": "Mars",
+                "HasRings": False,
+                "Radius": 2106,
+                "Moons": [{"Name": "Phobos"}, {"Name": "Deimos"}],
+            },
+        ]
 
-            for key in keys:
-                if keys[pygame.K_LEFT]:
-                    self.dirnx = -1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        print("Inserting items in the collection...")
+        for planet in planets:
+            container.create_item(planet)
+            print("\t'{0}' created".format(planet["id"]))
+        print("\tdone")
 
-                elif keys[pygame.K_RIGHT]:
-                    self.dirnx = 1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+    def simple_query(self, container):
+        print("Quering the container...")
+        items = list(
+            container.query_items(
+                query="SELECT c.id FROM c", enable_cross_partition_query=True
+            )
+        )
+        print("\tdone: {0}".format(items))
 
-                elif keys[pygame.K_UP]:
-                    self.dirnx = 0
-                    self.dirny = -1
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+    def delete_database(self):
+        print("Cleaning up the resource...")
+        self.client.delete_database(self.dbName)
+        print("\tdone")
 
-                elif keys[pygame.K_DOWN]:
-                    self.dirnx = 0
-                    self.dirny = 1
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+    def run(self):
+        print("")
+        print("------------------------")
+        print("Cosmos DB")
+        print("------------------------")
+        print("1) Create a Database")
+        print("2) Create a Container in the database")
+        print("3) Insert Documents (items) into the Container")
+        print("4) Delete Database (Clean up the resource)")
+        print("")
 
-        for i, c in enumerate(self.body):
-            p = c.pos[:]
-            if p in self.turns:
-                turn = self.turns[p]
-                c.move(turn[0],turn[1])
-                if i == len(self.body)-1:
-                    self.turns.pop(p)
-            else:
-                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
-                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
-                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
-                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
-                else: c.move(c.dirnx,c.dirny)
-        
+        # Ensure that the database does not exist
+        try:
+            self.delete_database()
+        except:
+            pass
 
-    def reset(self, pos):
-        self.head = cube(pos)
-        self.body = []
-        self.body.append(self.head)
-        self.turns = {}
-        self.dirnx = 0
-        self.dirny = 1
-
-
-    def addCube(self):
-        tail = self.body[-1]
-        dx, dy = tail.dirnx, tail.dirny
-
-        if dx == 1 and dy == 0:
-            self.body.append(cube((tail.pos[0]-1,tail.pos[1])))
-        elif dx == -1 and dy == 0:
-            self.body.append(cube((tail.pos[0]+1,tail.pos[1])))
-        elif dx == 0 and dy == 1:
-            self.body.append(cube((tail.pos[0],tail.pos[1]-1)))
-        elif dx == 0 and dy == -1:
-            self.body.append(cube((tail.pos[0],tail.pos[1]+1)))
-
-        self.body[-1].dirnx = dx
-        self.body[-1].dirny = dy
-        
-
-    def draw(self, surface):
-        for i, c in enumerate(self.body):
-            if i ==0:
-                c.draw(surface, True)
-            else:
-                c.draw(surface)
-
-
-def drawGrid(w, rows, surface):
-    sizeBtwn = w // rows
-
-    x = 0
-    y = 0
-    for l in range(rows):
-        x = x + sizeBtwn
-        y = y + sizeBtwn
-
-        pygame.draw.line(surface, (255,255,255), (x,0),(x,w))
-        pygame.draw.line(surface, (255,255,255), (0,y),(w,y))
-        
-
-def redrawWindow(surface):
-    global rows, width, s, snack
-    surface.fill((0,0,0))
-    s.draw(surface)
-    snack.draw(surface)
-    drawGrid(width,rows, surface)
-    pygame.display.update()
-
-
-def randomSnack(rows, item):
-
-    positions = item.body
-
-    while True:
-        x = random.randrange(rows)
-        y = random.randrange(rows)
-        if len(list(filter(lambda z:z.pos == (x,y), positions))) > 0:
-            continue
-        else:
-            break
-        
-    return (x,y)
-
-
-def message_box(subject, content):
-    root = tk.Tk()
-    root.attributes("-topmost", True)
-    root.withdraw()
-    messagebox.showinfo(subject, content)
-    try:
-        root.destroy()
-    except:
-        pass
-
-
-def main():
-    global width, rows, s, snack
-    width = 500
-    rows = 20
-    win = pygame.display.set_mode((width, width))
-    s = snake((255,0,0), (10,10))
-    snack = cube(randomSnack(rows, s), color=(0,255,0))
-    flag = True
-
-    clock = pygame.time.Clock()
-    
-    while flag:
-        pygame.time.delay(50)
-        clock.tick(10)
-        s.move()
-        if s.body[0].pos == snack.pos:
-            s.addCube()
-            snack = cube(randomSnack(rows, s), color=(0,255,0))
-
-        for x in range(len(s.body)):
-            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
-                print('Score: ', len(s.body))
-                message_box('You Lost!', 'Play again...')
-                s.reset((10,10))
-                break
-
-            
-        redrawWindow(win)
-
-        
-    pass
-
-
-
-main()
+        try:
+            db = self.create_database()
+            container = self.create_container(db=db)
+            self.create_documents(container=container)
+            self.simple_query(container=container)
+        finally:
+            # if something goes wrong, the resource should be cleaned anyway
+            self.delete_database()
