aa7049b5c5bc5dd9d4e064d1d629179cc4808295
L30N87 <54728140+L30N87@users.noreply.github.com>
2019-10-02 15:09:31 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-
-@app.route('/suma/<int:s1>/<int:s2>',methods=['GET'])
-def suma(s1,s2):
-    return str(s1+s2)
-
-if __name__ == '__main__':
+@app.route("/")
+def main():
+    return render_template('index.html')
+if __name__ == "__main__":
     app.run()
