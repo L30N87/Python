442159b309a44c66e0d340a95a081b349dbef2f1
L30N87 <54728140+L30N87@users.noreply.github.com>
2019-10-03 09:46:33 -0500

    Update application.py

--- application.py
+++ application.py
@@ -9,9 +9,7 @@ def main():
 def iPhone_X___2():
      return render_template('iPhone_X___2.html')    
 
-@app.route("/iPhone_X___3.html")
-def iPhone_X___3():
-     return render_template('iPhone_X___3.html')  
+
     
 
 if __name__ == "__main__":
